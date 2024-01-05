2d4209378e6422a4ef41f9f057d560f5833dcfa4
Nathan George <georgenathan010@gmail.com>
2024-01-05 22:21:12 +0300

    addition: links to social-media platforms

--- src/components/TypedData.jsx
+++ src/components/TypedData.jsx
@@ -11,9 +11,9 @@ const TypedData = () => {
       <h1 className='text-3xl text-[#181111] font-bold '>Meet Nathan George</h1>
       <h1 className='text-3xl text-[#141111] font-bold'>Web Developer and Coder</h1>
       <span className='flex justify-center gap-5 mt-4 text-[#021841fa]'>
-        <a href=""><GrLinkedin size={30}/></a>
+        <a href= "https://www.linkedin.com/in/nathan-george-mi" target='_blank' rel='noreferrer'><GrLinkedin size={30}/></a>
         <a href=""> <GrTwitter size={30} /> </a>
-        <a href=""> <GrGithub size={30}/> </a>
+        <a href="https://github.com/Nathami-n" target='_blank' rel='noreferrer'> <GrGithub size={30}/> </a>
       </span>
     </div>
   )

e546053352d225385a2bdecd63e054ef04d780e7
Nathan George <georgenathan010@gmail.com>
2024-01-14 01:13:21 +0300

    fixed the arrangement

--- src/Data.js
+++ src/Data.js
@@ -1,16 +1,17 @@
 export const data =  [
     {
         id: 1,
+        source: 'https://www.minibigtech.com/blog/wp-content/uploads/2016/12/homapage.jpg',
+        description: 'this is a youtube clone that I am working on too',
+        link: 'https://nathan-youtube-clone.vercel.app/',
+    }, 
+
+    {
+        id: 2,
         source: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202101/Screenshot_2021-01-22_at_3.54._1200x768.png?size=690:388',
         description: 'This is a project on a food store I am yet to work on. I will keep you updated',
         link: 'https://nathan-youtube-clone.vercel.app/',
     },
-    {
-        id: 2,
-        source: 'https://www.minibigtech.com/blog/wp-content/uploads/2016/12/homapage.jpg',
-        description: 'this is a youtube clone that I am working on too',
-        link: ''
-    }, 
     {
         id:3,
         source: 'https://neilpatel.com/wp-content/uploads/2023/06/Best_landing_pages3-700x397.jpg',

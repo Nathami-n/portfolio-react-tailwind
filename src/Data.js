a240ed928620abdc54aea3ca7ff6193d5529b69a
Nathan George <georgenathan010@gmail.com>
2024-01-14 00:51:49 +0300

    added link to my youtube project

--- src/Data.js
+++ src/Data.js
@@ -1,14 +1,14 @@
 export const data =  [
     {
         id: 1,
+        source: 'https://nathan-youtube-clone.vercel.app/',
+        description: 'this is a youtube clone that I am working on too',
+    }, 
+    {
+        id: 2,
         source: 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202101/Screenshot_2021-01-22_at_3.54._1200x768.png?size=690:388',
         description: 'This is a project on a food store I am yet to work on. I will keep you updated',
     },
-    {
-        id: 2,
-        source: 'https://www.minibigtech.com/blog/wp-content/uploads/2016/12/homapage.jpg',
-        description: 'this is a youtube clone that I am working on too',
-    }, 
     {
         id:3,
         source: 'https://neilpatel.com/wp-content/uploads/2023/06/Best_landing_pages3-700x397.jpg',

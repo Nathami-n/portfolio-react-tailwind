97efed2476640caaf51068dc15ed77e5b1b3922c
Nathan George <georgenathan010@gmail.com>
2024-03-08 17:46:17 +0300

    set up basic routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgbody:"#070707",
+        bgcard:"#111",
+      }
+    },
   },
   plugins: [],
 }
-

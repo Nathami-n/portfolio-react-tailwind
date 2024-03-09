d08216fbc79d8bb26c7bcb1af7185b26e590cf10
Nathan George <georgenathan010@gmail.com>
2024-03-09 10:48:59 +0300

    added the sidenav for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,44 @@
 import { useState } from "react";
 import { VscMenu, VscChromeClose } from "react-icons/vsc";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 const Header = () => {
   const [openNav, setOpenNav] = useState(false);
   return (
-    <nav className="w-full p-4">
-      <div className="flex justify-between items-center">
-        <div>
-          <span className="font-bold text-white text-2xl">Nathan.</span>
-        </div>
-        <div onClick={() => setOpenNav(!openNav)}>
-          {openNav ? (
-            <motion.button whileTap={{ opacity: 0, x: "300px" }}>
-              {" "}
+    <div className="w-full ">
+      <nav className="w-full p-5">
+        <div className="flex justify-between items-center">
+          <div>
+            <span className="font-bold text-white text-2xl">Nathan.</span>
+          </div>
+          <motion.button onClick={() => setOpenNav(!openNav)}>
+            {!openNav ? (
               <VscMenu className="text-white text-3xl cursor-pointer" />
-            </motion.button>
-          ) : (
-            <motion.button
-              whileTap={{
-                opacity: `${openNav ? 1 : 0}`,
-                x: [-100, -50, -25, 0],
-              }}
-            >
-              {" "}
+            ) : (
               <VscChromeClose className="text-white text-3xl cursor-pointer" />
-            </motion.button>
-          )}
+            )}
+          </motion.button>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {openNav && (
+        <div className="border-black mx-auto bg-black/30 relative flex justify-center items-center z-2">
+          <ul className="p-4 flex flex-col gap-12 bottom-0  h-[360px] w-[400px] items-center fixed  rounded-xl  bg-bgcard z-10">
+            <li className="nav">
+              <Link>Home</Link>
+            </li>
+            <li className="nav">
+              <Link>Work</Link>
+            </li>
+            <li className="nav">
+              <Link>About</Link>
+            </li>
+            <li className="nav">
+              <Link>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 

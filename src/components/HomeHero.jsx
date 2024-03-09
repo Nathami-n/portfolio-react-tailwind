aaa5e3971756e8adf9eb76abeabf2b4e1be77096
Nathan George <georgenathan010@gmail.com>
2024-03-10 00:48:19 +0300

    allowed the text content box to flow automatically

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,13 +1,18 @@
 const HomeHero = () => {
   return (
-    <section className="h-screen ">
-      <div className=" flex flex-col items-center mt-8 gap-11">
-        <div className="w-[450px] h-[300px] bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
-          <img src="/public/naate.png" alt="Hero Image" className="object-contain w-full h-full" />
+    <section className="h-screen max-md:p-6">
+      <div className=" flex flex-col items-center  mt-8 gap-11">
+        <div className="sm:w-[450px] max-sm:w-[300px]  max-sm:h-[300px] sm:h-[300px] bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
+          <img src="/public/nate.svg" alt="Hero Image" className="object-contain w-full h-full" />
         </div>
-        <div className="text-white w-[450px] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
+        <div className="text-white max-w-[400px] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
             <h1 className="font-bold text-4xl">Hello, I&apos;m <span className="text-[#f75c2d]">Nathan</span></h1>
             <p className="text-[#cdcdcd]">&#8212; A tech oriented enthusiasit who is characterised avid and constant devouring  of new and existing technologies. I am well versed in both front-end and back-end technologies  </p>
+            <div>
+                <div> 
+                    <img src="/react.svg" alt="react-icon" className="w-[30px] bg-white/50 rounded-full" />
+                </div>
+            </div>
         </div>
       </div>
     </section>

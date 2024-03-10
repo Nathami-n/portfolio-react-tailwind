f6de5196d174ca01597328369afb4c0ccc8c7927
Nathan George <georgenathan010@gmail.com>
2024-03-10 09:48:32 +0300

    added the responsiveness and transition from flex  to grid

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -4,7 +4,7 @@ const HomeHero = () => {
       <div className="  mx-auto max-lg:flex max-lg:flex-col items-center lg:gap-[40px] lg:grid lg:grid-cols-8  mt-8 gap-11">
         <div className="  w-[90%] h-auto lg:w-[95%] lg:h-[600px]  lg:ml-[40px] lg:col-span-4 bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
           <img
-            src="/public/nate.svg"
+            src="/nate.svg"
             alt="Hero Image"
             className="object-contain w-full h-full"
           />
@@ -14,7 +14,7 @@ const HomeHero = () => {
             Hello, I&apos;m <span className="text-[#f75c2d]">Nathan</span>
           </h1>
           <p className="text-[#cdcdcd]">
-            &#8212; A tech oriented enthusiasit who is characterised avid and
+            &#8212; A tech oriented enthusiasit who is characterised by avid and
             constant devouring of new and existing technologies. I am well
             versed in both front-end and back-end technologies{" "}
           </p>

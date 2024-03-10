c235af675afc5ff71efff2e42350b831bf362ec6
Nathan George <georgenathan010@gmail.com>
2024-03-10 07:53:22 +0300

    made the card responsive on small devices

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -2,17 +2,31 @@ const HomeHero = () => {
   return (
     <section className="h-screen max-md:p-6">
       <div className=" flex flex-col items-center  mt-8 gap-11">
-        <div className="sm:w-[450px] max-sm:w-[300px]  max-sm:h-[300px] sm:h-[300px] bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
-          <img src="/public/nate.svg" alt="Hero Image" className="object-contain w-full h-full" />
+        <div className=" max-sm:w-[80%] max-sm:h-auto bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
+          <img
+            src="/public/nate.svg"
+            alt="Hero Image"
+            className="object-contain w-full h-full"
+          />
         </div>
-        <div className="text-white max-w-[400px] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
-            <h1 className="font-bold text-4xl">Hello, I&apos;m <span className="text-[#f75c2d]">Nathan</span></h1>
-            <p className="text-[#cdcdcd]">&#8212; A tech oriented enthusiasit who is characterised avid and constant devouring  of new and existing technologies. I am well versed in both front-end and back-end technologies  </p>
+        <div className="text-white max-sm:h-[450px]] max-w-[90%] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
+          <h1 className="font-bold text-4xl">
+            Hello, I&apos;m <span className="text-[#f75c2d]">Nathan</span>
+          </h1>
+          <p className="text-[#cdcdcd]">
+            &#8212; A tech oriented enthusiasit who is characterised avid and
+            constant devouring of new and existing technologies. I am well
+            versed in both front-end and back-end technologies{" "}
+          </p>
+          <div>
             <div>
-                <div> 
-                    <img src="/react.svg" alt="react-icon" className="w-[30px] bg-white/50 rounded-full" />
-                </div>
+              <img
+                src="/react.svg"
+                alt="react-icon"
+                className="w-[30px] bg-white/50 rounded-full"
+              />
             </div>
+          </div>
         </div>
       </div>
     </section>

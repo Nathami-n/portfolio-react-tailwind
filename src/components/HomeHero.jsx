b092db2282ca035fdddddd12b8e604d6f1d4c7e2
Nathan George <georgenathan010@gmail.com>
2024-03-10 08:02:47 +0300

    added percentages as the values

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -2,14 +2,14 @@ const HomeHero = () => {
   return (
     <section className="h-screen max-md:p-6">
       <div className=" flex flex-col items-center  mt-8 gap-11">
-        <div className=" max-sm:w-[80%] max-sm:h-auto bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
+        <div className=" w-[90%] h-auto bg-bgcard rounded-2xl  border border-white/20 flex flex-col justify-center items-center">
           <img
             src="/public/nate.svg"
             alt="Hero Image"
             className="object-contain w-full h-full"
           />
         </div>
-        <div className="text-white max-sm:h-[450px]] max-w-[90%] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
+        <div className="text-white h-auto w-[90%] gap-10 bg-bgcard rounded-2xl flex flex-col p-8 justify-center items-center">
           <h1 className="font-bold text-4xl">
             Hello, I&apos;m <span className="text-[#f75c2d]">Nathan</span>
           </h1>

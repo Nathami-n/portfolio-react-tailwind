3c40a5a66a38dd72c7d93b3e65d2748bb08349a1
Nathan George <georgenathan010@gmail.com>
2024-03-11 17:50:19 +0300

    changed the row span for the project

--- src/components/HomeProjects.jsx
+++ src/components/HomeProjects.jsx
@@ -2,7 +2,7 @@ const HomeProjects = () => {
   return (
     <section className="min-h-screen ">
       <div className="max-sm:grid  max-sm:grid-cols-7  max-sm:gap-8 max-sm:p-4 min-h-screen max-sm:w-full">
-        <div className="max-sm:row-span-3 max-sm:col-span-7  relative h-full w-full ">
+        <div className="max-sm:col-span-7  relative h-full w-full ">
           <div className=" max-sm:rounded-lg w-full h-full bg-youtube-card bg-cover bg-center bg-no-repeat cursor-pointer "></div>
         </div>
         <div className="card  max-sm:col-span-7 relative h-full ">

e3f2471a48005df3b3268f012dd946db85560c63
Nathan George <georgenathan010@gmail.com>
2024-03-14 23:27:04 +0300

    added marquee with my technologies

--- src/components/HomeTools.jsx
+++ src/components/HomeTools.jsx
@@ -1,14 +1,15 @@
-
-
+import Marquee from "react-fast-marquee";
 const HomeTools = () => {
   return (
-    <section 
-    className="
+    <section
+      className="
     w-full
     h-screen
     mt-10
-    ">
-        <div className="
+    "
+    >
+      <div
+        className="
         h-full
         w-full
         flex
@@ -16,58 +17,272 @@ const HomeTools = () => {
         gap-3
         p-3
         relative
-        ">
-            <div className="
+        "
+      >
+        <div
+          className="
             card
             h-[350px]
             p-6
-            ">
-                <h1 className="
+            "
+        >
+          <h1
+            className="
                 text-3xl
                 text-white
                 font-bold
                 mb-6
-                ">
-                    *About <span className="
+                "
+          >
+            *About{" "}
+            <span
+              className="
                     text-orange-600
-                    ">
-                        me</span>
-                </h1>
-                <p className="
+                    "
+            >
+              me
+            </span>
+          </h1>
+          <p
+            className="
                 text-[#cdcdcd]
                 text-lg
                 mt-[50px]
-                ">
-                    Want to know about me and my skillset? Click on the card that you are hovering hover
-                    Click on the card
-                </p>
-                <div className="
+                "
+          >
+            Want to know about me and my skillset? Click on the card that you
+            are hovering hover Click on the card
+          </p>
+          <div
+            className="
                 text-white
                 absolute
                 right-5
                 top-[300px]
             
-                ">button</div>
-            </div>
-            <div className="
+                "
+          >
+            button
+          </div>
+        </div>
+        <div
+          className="
             card
             h-[350px]
             p-3
-            ">
-                <h1 className="
+            flex
+            flex-col
+            gap-9
+            "
+        >
+          <h1
+            className="
                 text-white
                 font-bold
                 text-3xl
                 text-center
                 "
-                >Technologies I <span className="
+          >
+            Technologies I{" "}
+            <span
+              className="
                 text-orange-600
-                ">use</span></h1>
+                "
+            >
+              use
+            </span>
+          </h1>
+          <div
+            className="
+            mt-9
+            mx-auto
+            w-[70%]
+            
+            "
+          >
+            <Marquee pauseOnHover>
+              <div
+                className="
+           flex
+           gap-12
+           "
+              >
+                <div
+                  className="
+                    text-blue-300
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+                    ml-8
 
+                    "
+                >
+                  React
+                </div>
+                <div
+                  className="
+                    text-sky-500
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    cursor-pointer
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                >
+                  NextJs
+                </div>
+                <div
+                  className="
+                    text-purple-400
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    rounded-xl
+                    shadow-md
+                    shadow-orange-600
+
+                    "
+                >
+                  Tailwindcss
+                </div>
+                <div
+                  className="
+                    text-rose-500
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    rounded-lg
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                >
+                  Node
+                </div>
+                <div
+                  className="
+                    text-green-400
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                >
+                  MongoDB
+                </div>
+              </div>
+            </Marquee>
+            <div
+              className="
+    mt-3
+    "
+            >
+              <Marquee autoFill pauseOnHover direction="right">
+                <div
+                  className="
+            flex
+            gap-12
+            "
+                >
+                  <div
+                    className="
+                    text-yellow-600
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-5
+                    m-1
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+                    ml-8
+
+                    "
+                  >
+                    SQL
+                  </div>
+                  <div
+                    className="
+                    text-orange-800
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    m-1
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                  >
+                    JAVASCRIPT
+                  </div>
+                  <div
+                    className="
+                    text-purple-900
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                  >
+                    REDUX
+                  </div>
+                  <div
+                    className="
+                    text-rose-900
+                    text-xl
+                    font-bold
+                    uppercase
+                    border
+                    p-4
+                    rounded-xl
+                    shadow-md
+                    shadow-blue-400
+
+                    "
+                  >
+                    C/C++/C#/JAVA
+                  </div>
+                </div>
+              </Marquee>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeTools
+export default HomeTools;
